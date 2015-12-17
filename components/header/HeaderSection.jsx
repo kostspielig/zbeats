06120d184a6a04a7f29c75e8150d63fad22df77c
mcarrasco <maria.carrasco@zalando.de>
2015-12-17 19:52:49 +0100

    Get global theme from app

--- components/header/HeaderSection.jsx
+++ components/header/HeaderSection.jsx
@@ -4,7 +4,6 @@ import AppBar from 'material-ui/lib/app-bar'
 import IconButton from 'material-ui/lib/icon-button'
 import ThemeManager from 'material-ui/lib/styles/theme-manager'
 import ThemeDecorator from 'material-ui/lib/styles/theme-decorator'
-import LightRawTheme from 'material-ui/lib/styles/raw-themes/light-raw-theme'
 
 import IconMenu from 'material-ui/lib/menus/icon-menu'
 import MenuItem from 'material-ui/lib/menus/menu-item'
@@ -12,7 +11,6 @@ import TextField from 'material-ui/lib/text-field'
 import FlatButton from 'material-ui/lib/flat-button'
 import Slider from 'material-ui/lib/slider'
 
-@ThemeDecorator(ThemeManager.getMuiTheme(LightRawTheme))
 class HeaderSection extends Component {
 
     changeBpm(e, value) {
